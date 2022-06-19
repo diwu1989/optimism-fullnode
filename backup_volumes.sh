efc872db906487ceb6927df836242439d13c149d
Di Wu <dw323@cornell.edu>
2022-06-19 14:07:55 -0700

    backup volume

---
+++ backup_volumes.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+export DTL_SNAPSHOT="/tmp/op-replica_dtl-$(date -I).tar.zst"
+echo "Archiving dtl volume to $DTL_SNAPSHOT"
+(
+  cd /var/lib/docker/volumes/op-replica_dtl/_data
+  tar --zstd -cvf $DTL_SNAPSHOT .
+)
+
+export GETH_SNAPSHOT="/tmp/op-replica_geth-$(date -I).tar.zst"
+echo "Archiving geth volume to $GETH_SNAPSHOT"
+(
+  cd /var/lib/docker/volumes/op-replica_geth/_data
+  tar --zstd -cvf $GETH_SNAPSHOT geth
+)
