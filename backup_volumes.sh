1e7fd0d6206805c5bb1b6c42bc7a4d55c71a5aad
Di Wu <dw323@cornell.edu>
2022-06-19 14:15:25 -0700

    f

--- backup_volumes.sh
+++ backup_volumes.sh
@@ -8,12 +8,14 @@ export DTL_SNAPSHOT="/tmp/op-replica_dtl-$(date -I).tar.zst"
 echo "Archiving dtl volume to $DTL_SNAPSHOT"
 (
   cd /var/lib/docker/volumes/op-replica_dtl/_data
-  tar --zstd -cvf $DTL_SNAPSHOT .
+  tar --zstd -cvf "$DTL_SNAPSHOT" .
 )
 
 export GETH_SNAPSHOT="/tmp/op-replica_geth-$(date -I).tar.zst"
 echo "Archiving geth volume to $GETH_SNAPSHOT"
 (
   cd /var/lib/docker/volumes/op-replica_geth/_data
-  tar --zstd -cvf $GETH_SNAPSHOT geth
+  tar --zstd -cvf "$GETH_SNAPSHOT" geth
 )
+
+ls -alh "$DTL_SNAPSHOT" "$GETH_SNAPSHOT"
